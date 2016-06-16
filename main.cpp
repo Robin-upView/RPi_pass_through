fd32cc0dae660284b54163d3b6d6aa37c79c6cea
Robin-upView <robin.gojon@gmail.com>
2016-06-16 22:02:59 +0100

    first commit
    this is the first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Robin
+ *
+ * Created on 16 juin 2016, 21:35
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
